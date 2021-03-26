82e7ba942537c50cd54177db42e892aa67fbe472
SammyFS <mgkiddrock@gmail.com>
2021-03-27 01:08:27 +0700

    Update soal3a.sh
    bisa dicek dulu rek

--- soal3/soal3a.sh
+++ soal3/soal3a.sh
@@ -1,19 +1,44 @@
 #!/bin/bash
+data_namafile () {
+if [ $1 -le 9 ]
+then
+        namafile="Koleksi_0$1.jpg"
+fi
+}
 
-for ((num=1; num<=23; num++))
+for ((i=1; i<=23; i++))
 do
-        wget -O "Koleksi_$num.jpg" https://loremflickr.com/320/240/kitten 2>> Foto.log 
+        wget -a Foto.log https://loremflickr.com/320/240/kitten -O "Koleksi_$i.jpg"
+        for ((j=1; j<i; j++))
+        do
+ 	      	if comm Koleksi_$i.jpg Koleksi_$j.jpg &> /dev/null
+                then
+                        rm Koleksi_$i.jpg
+                        break
+                fi
+        done
 done
 
 for ((i=1; i<=23; i++))
 do
-        for ((j=i+1; j<=23; j++))
-        do
-                cek=$(cmp "Koleksi_$i.jpg" "Koleksi_$j.jpg")
-                beda=$?
-                if [ $beda -eq 0 ]
-                then
-                        rm Koleksi_$j.jpg
-                fi
-        done
+	if [ ! -f Koleksi_$i.jpg ];
+	then
+		for ((j=23; j>i; j--))
+		do
+			if [ -f Koleksi_$j.jpg ];
+			then
+				mv Koleksi_$j.jpg Koleksi_$i.jpg
+				break
+			fi
+		done
+	fi
+done
+
+for ((i=1; i<10; i=i+1))
+do
+	data_namafile "$i"
+	if [ -f Koleksi_$i.jpg ]
+	then
+		mv Koleksi_$i.jpg $namafile
+	fi
 done
